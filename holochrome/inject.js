1e1cdb8e14e8ccbcba3468f544c0e1aaa1311db4
Noah <noah.yonack@gmail.com>
2016-07-25 20:09:35 -0400

    Wrapped script.js in an IIFE

--- holochrome/inject.js
+++ holochrome/inject.js
@@ -1,6 +1,8 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('disableReload.js');
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(s);
+(function() {
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('disableReload.js');
+    s.onload = function() {
+        this.parentNode.removeChild(this);
+    };
+    (document.head || document.documentElement).appendChild(s);
+})()
